a5735279a73b18be3beb969f36e3c8da9a8215e4
SecurityPlz <matt@n00bs3c.com>
2022-01-23 14:51:36 -0600

    safety commit ahead of main.go rework

--- ip-interrogate/source/hello/go.mod
+++ ip-interrogate/source/hello/go.mod
@@ -2,4 +2,7 @@ module github.com/SecurityPlz/golang-api/tree/master/ip-interrogate/source/hello
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.24.0
+require (
+	github.com/aws/aws-lambda-go v1.24.0
+	github.com/likexian/whois v1.12.4
+)
---
+++ ip-interrogate/source/hello/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	// ErrNameNotProvided is thrown when a name is not provided
+	HTTPMethodNotSupported = errors.New("no name was provided in the HTTP body")
+)
+
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Body size = %d. \n", len(request.Body))
+	fmt.Println("Headers:")
+	for key, value := range request.Headers {
+		fmt.Printf("  %s: %s\n", key, value)
+	}
+	if request.HTTPMethod == "GET" {
+		fmt.Printf("GET METHOD\n")
+		return events.APIGatewayProxyResponse{Body: "BUTT", StatusCode: 200}, nil
+	} else if request.HTTPMethod == "POST" {
+		fmt.Printf("POST METHOD\n")
+		return events.APIGatewayProxyResponse{Body: "POST", StatusCode: 200}, nil
+	} else {
+		fmt.Printf("NEITHER\n")
+		return events.APIGatewayProxyResponse{}, HTTPMethodNotSupported
+	}
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+/*
+func main() {
+	var first string
+	fmt.Scanln(&first)
+	resp, err := whois.Whois(first)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(resp)
+}
+
+	"log"
+
+	"github.com/likexian/whois"
+*/
