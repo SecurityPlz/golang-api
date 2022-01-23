8a0366036d6828af428e00a6d35d19885156d621
SecurityPlz <matt@n00bs3c.com>
2022-01-23 11:38:52 -0600

    simplified  codebase to use just one go file

--- ip-interrogate/infrastructure/main.tf
+++ ip-interrogate/infrastructure/main.tf
@@ -16,10 +16,8 @@ data "aws_caller_identity" "current" {}
 
 locals {
   account_id      = data.aws_caller_identity.current.account_id
-  environment     = "dev"
-  lambda_handler  = "hello"
+  lambda_handler  = "main"
   name            = "ip-interrogate"
-  random_name     = "Alice"
   region          = "us-east-2"
 }
 
@@ -166,7 +164,7 @@ resource "aws_api_gateway_gateway_response" "internal_server_error" {
 resource "aws_api_gateway_resource" "endpoint" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "hello"
+  path_part   = "/"
 }
 
 resource "aws_api_gateway_method" "endpoint" {
